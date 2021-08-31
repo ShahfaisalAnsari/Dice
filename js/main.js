d16c8abb3cfb2e5ebc7cda6db1da00753b4dacd4
Shahfaisal <faisalkht@gmail.com>
2021-08-31 14:21:58 +0530

    remove btn

--- js/main.js
+++ js/main.js
@@ -67,55 +67,55 @@ dice.addEventListener("click",function(){
 
 // btn----------------------------------------
 
-btn.addEventListener("click", function () {
-    var num = Math.round(Math.random() * 5 + 1);
-    if (num == 1) {
-        one.style.visibility = "visible"
-        two.style.visibility = "hidden"
-        three.style.visibility = "hidden"
-        four.style.visibility = "hidden"
-        five.style.visibility = "hidden"
-        six.style.visibility = "hidden"
-    }
-    else if (num == 2) {
-        one.style.visibility = "hidden"
-        two.style.visibility = "visible"
-        three.style.visibility = "hidden"
-        four.style.visibility = "hidden"
-        five.style.visibility = "hidden"
-        six.style.visibility = "hidden"
-    }
-    else if (num == 3) {
-        one.style.visibility = "hidden"
-        two.style.visibility = "hidden"
-        three.style.visibility = "visible"
-        four.style.visibility = "hidden"
-        five.style.visibility = "hidden"
-        six.style.visibility = "hidden"
-    }
-    else if (num == 4) {
-        one.style.visibility = "hidden"
-        two.style.visibility = "hidden"
-        three.style.visibility = "hidden"
-        four.style.visibility = "visible"
-        five.style.visibility = "hidden"
-        six.style.visibility = "hidden"
-    }
-    else if (num == 5) {
-        one.style.visibility = "hidden"
-        two.style.visibility = "hidden"
-        three.style.visibility = "hidden"
-        four.style.visibility = "hidden"
-        five.style.visibility = "visible"
-        six.style.visibility = "hidden"
-    }
-    else if (num == 6) {
-        one.style.visibility = "hidden"
-        two.style.visibility = "hidden"
-        three.style.visibility = "hidden"
-        four.style.visibility = "hidden"
-        five.style.visibility = "hidden"
-        six.style.visibility = "visible"
-    }
+// btn.addEventListener("click", function () {
+//     var num = Math.round(Math.random() * 5 + 1);
+//     if (num == 1) {
+//         one.style.visibility = "visible"
+//         two.style.visibility = "hidden"
+//         three.style.visibility = "hidden"
+//         four.style.visibility = "hidden"
+//         five.style.visibility = "hidden"
+//         six.style.visibility = "hidden"
+//     }
+//     else if (num == 2) {
+//         one.style.visibility = "hidden"
+//         two.style.visibility = "visible"
+//         three.style.visibility = "hidden"
+//         four.style.visibility = "hidden"
+//         five.style.visibility = "hidden"
+//         six.style.visibility = "hidden"
+//     }
+//     else if (num == 3) {
+//         one.style.visibility = "hidden"
+//         two.style.visibility = "hidden"
+//         three.style.visibility = "visible"
+//         four.style.visibility = "hidden"
+//         five.style.visibility = "hidden"
+//         six.style.visibility = "hidden"
+//     }
+//     else if (num == 4) {
+//         one.style.visibility = "hidden"
+//         two.style.visibility = "hidden"
+//         three.style.visibility = "hidden"
+//         four.style.visibility = "visible"
+//         five.style.visibility = "hidden"
+//         six.style.visibility = "hidden"
+//     }
+//     else if (num == 5) {
+//         one.style.visibility = "hidden"
+//         two.style.visibility = "hidden"
+//         three.style.visibility = "hidden"
+//         four.style.visibility = "hidden"
+//         five.style.visibility = "visible"
+//         six.style.visibility = "hidden"
+//     }
+//     else if (num == 6) {
+//         one.style.visibility = "hidden"
+//         two.style.visibility = "hidden"
+//         three.style.visibility = "hidden"
+//         four.style.visibility = "hidden"
+//         five.style.visibility = "hidden"
+//         six.style.visibility = "visible"
+//     }
 
-});
+// });
